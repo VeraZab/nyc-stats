49bddbd631b8d2fdbc63ec2de0f11f06e1ca70bb
VeraZab <vera.zabeida@gmail.com>
2023-03-13 07:53:58 -0400

    remove not needed resources

--- terraform/artifact-registry.tf
+++ terraform/artifact-registry.tf
@@ -1,11 +1,5 @@
-resource "google_artifact_registry_repository" "flow-images" {
-  repository_id = "flow-images"
-  location      = var.GCP_REGION
-  format        = "docker"
-}
-
-resource "google_artifact_registry_repository" "agent-images" {
-  repository_id = "agent-images"
+resource "google_artifact_registry_repository" "nyc-stats" {
+  repository_id = "nyc-stats"
   location      = var.GCP_REGION
   format        = "docker"
 }
