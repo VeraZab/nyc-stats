2e9a13ebc8f39bd9f670664aca2e765a9b5b4350
VeraZab <vera.zabeida@gmail.com>
2023-03-21 21:39:05 -0400

    use offset in description instead of print statement

--- flows/pipeline.py
+++ flows/pipeline.py
@@ -59,7 +59,7 @@ def convert_to_df(results):
     return df
 
 
-@task(task_run_name="extracting from {from_date} (inclusive) to {to_date} (inclusive)")
+@task(task_run_name="extracting current offset: {offset}")
 def extract(results_per_page, offset, from_date, to_date):
     response = requests.get(
         f"https://data.cityofnewyork.us/resource/erm2-nwe9.json?&$limit={results_per_page}&$offset={offset}&$where=created_date between '{from_date}T00:00:00' and '{to_date}T23:59:59'"
@@ -74,7 +74,6 @@ def extract_and_load(from_date, to_date):
     results = extract(results_per_page, offset, from_date, to_date)
 
     while len(results):
-        print(f"current offset: {offset}")
         offset += results_per_page
         df = convert_to_df(results)
         load(df)
