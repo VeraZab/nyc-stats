c0e2d4fddf1b3a8c7bfce948af012c14122f42d9
VeraZab <vera.zabeida@gmail.com>
2023-03-22 16:01:55 -0400

    edit order & some other adj

--- flows/pipeline.py
+++ flows/pipeline.py
@@ -51,6 +51,8 @@ def convert_to_df(results):
     df["created_date"] = pd.to_datetime(df["created_date"], errors="coerce")
     df["resolution_action_updated_date"] = pd.to_datetime(df["resolution_action_updated_date"], errors="coerce")
     df["closed_date"] = pd.to_datetime(df["closed_date"], errors="coerce")
+    df["unique_key"] = pd.to_numeric(df["unique_key"], errors="coerce")
+    df["incident_zip"] = pd.to_numeric(df["incident_zip"], errors="coerce")
     df["x_coordinate_state_plane"] = pd.to_numeric(df["x_coordinate_state_plane"], errors="coerce")
     df["y_coordinate_state_plane"] = pd.to_numeric(df["y_coordinate_state_plane"], errors="coerce")
     df["longitude"] = pd.to_numeric(df["longitude"], errors="coerce")
@@ -59,10 +61,10 @@ def convert_to_df(results):
     return df
 
 
-@task(task_run_name="extracting current offset: {offset}", retries=3)
+@task(task_run_name="extracting current offset: {offset}", retries=3, retry_delay_seconds=60)
 def extract(results_per_page, offset, from_date, to_date):
     response = requests.get(
-        f"https://data.cityofnewyork.us/resource/erm2-nwe9.json?$limit={results_per_page}&$offset={offset}&$where=created_date between '{from_date}T00:00:00' and '{to_date}T23:59:59'&$order=created_date ASC"
+        f"https://data.cityofnewyork.us/resource/erm2-nwe9.json?$limit={results_per_page}&$offset={offset}&$where=created_date between '{from_date}T00:00:00' and '{to_date}T23:59:59'&$order:id"
     )
     return response.json()
 
