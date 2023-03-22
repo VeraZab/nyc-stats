da9ef606b2924226474f9cb011eea3e0d7d28ab9
VeraZab <vera.zabeida@gmail.com>
2023-03-22 13:39:39 -0400

    reduce chunk size, and add order

--- flows/pipeline.py
+++ flows/pipeline.py
@@ -62,14 +62,14 @@ def convert_to_df(results):
 @task(task_run_name="extracting current offset: {offset}", retries=3)
 def extract(results_per_page, offset, from_date, to_date):
     response = requests.get(
-        f"https://data.cityofnewyork.us/resource/erm2-nwe9.json?&$limit={results_per_page}&$offset={offset}&$where=created_date between '{from_date}T00:00:00' and '{to_date}T23:59:59'"
+        f"https://data.cityofnewyork.us/resource/erm2-nwe9.json?$limit={results_per_page}&$offset={offset}&$where=created_date between '{from_date}T00:00:00' and '{to_date}T23:59:59'&$order=created_date ASC"
     )
     return response.json()
 
 
 @flow(log_prints=True, name="extracting and loading")
 def extract_and_load(from_date, to_date):
-    results_per_page = 100000
+    results_per_page = 10000
     offset = 0
     results = extract(results_per_page, offset, from_date, to_date)
 
